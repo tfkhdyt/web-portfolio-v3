edf67c1ae71115128185240c12c614c7861473ed
tfkhdyt <tfkhdyt@pm.me>
2022-08-29 09:29:02 +0700

    feat(_app): setup mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+
+import { MantineProvider } from '@mantine/core';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{ colorScheme: 'light' }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
