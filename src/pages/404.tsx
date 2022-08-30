4082c1f37d15f807188eded943c459bf0b4fb1dc
tfkhdyt <tfkhdyt@pm.me>
2022-08-30 17:54:37 +0700

    feat(pages): revamps 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,30 +1,86 @@
-import { Box, Button, Stack, Title } from '@mantine/core';
+import {
+  Button,
+  Center,
+  Container,
+  createStyles,
+  Group,
+  Text,
+  Title,
+} from '@mantine/core';
 import Head from 'next/head';
-import Image from 'next/image';
 import Link from 'next/link';
-import { IoMdArrowBack } from 'react-icons/io';
 
-function My404() {
+const useStyles = createStyles((theme) => ({
+  root: {
+    paddingTop: 80,
+    paddingBottom: 80,
+    paddingLeft: 30,
+    paddingRight: 30,
+  },
+
+  label: {
+    textAlign: 'center',
+    fontWeight: 900,
+    fontSize: 220,
+    lineHeight: 1,
+    marginBottom: theme.spacing.xl * 1.5,
+    color:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[4]
+        : theme.colors.gray[2],
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: 120,
+    },
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    textAlign: 'center',
+    fontWeight: 900,
+    fontSize: 38,
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: 32,
+    },
+  },
+
+  description: {
+    maxWidth: 500,
+    margin: 'auto',
+    marginTop: theme.spacing.xl,
+    marginBottom: theme.spacing.xl * 1.5,
+  },
+}));
+
+export default function NotFoundTitle() {
+  const { classes } = useStyles();
+
   return (
-    <>
+    <Center sx={{ width: '100vw', height: '100vh' }}>
       <Head>
         <title>404 Not Found</title>
       </Head>
-      <Box sx={{ display: 'grid', height: '100vh', placeItems: 'center' }}>
-        <Stack align='center' spacing={0}>
-          <Image src='/images/404.svg' alt='404' height={300} width={300} />
-          <Title order={1} mb='xl'>
-            Page not found
-          </Title>
+      <Container className={classes.root}>
+        <div className={classes.label}>404</div>
+        <Title className={classes.title}>You have found a secret place.</Title>
+        <Text
+          color='dimmed'
+          size='lg'
+          align='center'
+          className={classes.description}
+        >
+          Unfortunately, this is only a 404 page. You may have mistyped the
+          address, or the page has been moved to another URL.
+        </Text>
+        <Group position='center'>
           <Link href='/' passHref>
-            <Button color='violet' component='a' leftIcon={<IoMdArrowBack />}>
-              Back to home
+            <Button component='a' variant='subtle' size='md'>
+              Take me back to home page
             </Button>
           </Link>
-        </Stack>
-      </Box>
-    </>
+        </Group>
+      </Container>
+    </Center>
   );
 }
-
-export default My404;
