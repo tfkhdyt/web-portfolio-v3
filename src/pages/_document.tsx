168707fd4f92d5419c674f5bd85fbbbb1741feae
tfkhdyt <tfkhdyt@pm.me>
2022-08-30 14:53:29 +0700

    perf(font): set font proxy to fontbit.io

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,7 +13,7 @@ export default class _Document extends Document {
       <Html>
         <Head>
           <link
-            href='https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap'
+            href='https://fontbit.io/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap'
             rel='stylesheet'
           />
           <MetaTags />
