c9ef26af6c46b19847458eeff34a38408b80d2bb
tfkhdyt <tfkhdyt@pm.me>
2022-09-02 07:53:02 +0700

    feat(_app): add Jotai Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ import {
 import { useColorScheme, useHotkeys, useLocalStorage } from '@mantine/hooks';
 import { NotificationsProvider } from '@mantine/notifications';
 import { AnimatePresence, domAnimation, LazyMotion, m } from 'framer-motion';
+import { Provider } from 'jotai';
 import type { AppProps } from 'next/app';
 import { Router } from 'next/router';
 import { DefaultSeo } from 'next-seo';
@@ -48,65 +49,68 @@ function MyApp({ Component, pageProps, router }: AppProps) {
         theme={{ colorScheme, fontFamily: 'Source Sans Pro, sans-serif' }}
       >
         <NotificationsProvider>
-          <Layout>
-            <style>
-              {`html {
+          <Provider>
+            <Layout>
+              <style>
+                {`html {
                 color-scheme: ${colorScheme}
               }`}
-            </style>
-            <LazyMotion features={domAnimation}>
-              <AnimatePresence
-                mode='wait'
-                initial={false}
-                onExitComplete={() => window.scrollTo(0, 0)}
-              >
-                <m.div
-                  key={router.asPath}
-                  variants={variants}
-                  initial='hidden'
-                  animate='enter'
-                  exit='exit'
-                  transition={{ ease: 'easeInOut', duration: 0.3 }}
+              </style>
+              <LazyMotion features={domAnimation}>
+                <AnimatePresence
+                  mode='wait'
+                  initial={false}
+                  onExitComplete={() => window.scrollTo(0, 0)}
                 >
-                  <DefaultSeo
-                    titleTemplate='%s - Taufik Hidayat'
-                    defaultTitle={metaTagsData.title}
-                    description={metaTagsData.deskripsi}
-                    twitter={{
-                      cardType: 'summary_large_image',
-                      site: '@tfkhdyt142',
-                      handle: '@tfkhdyt142',
-                    }}
-                    canonical={metaTagsData.link}
-                    openGraph={{
-                      url: metaTagsData.link,
-                      type: 'profile',
-                      title: metaTagsData.title,
-                      description: metaTagsData.deskripsi,
-                      images: [
-                        {
-                          url: metaTagsData.link + '/images/thumbnail-630.png',
-                          width: 1120,
-                          height: 630,
-                          alt: 'thumbnail 630p',
-                          type: 'image/png'
-                        },
-                        {
-                          url: metaTagsData.link + metaTagsData.thumbnail,
-                          width: 1280,
-                          height: 720,
-                          alt: 'thumbnail 720p',
-                          type: 'image/png'
-                        },
-                      ],
-                      site_name: metaTagsData.title,
-                    }}
-                  />
-                  <Component {...pageProps} />
-                </m.div>
-              </AnimatePresence>
-            </LazyMotion>
-          </Layout>
+                  <m.div
+                    key={router.asPath}
+                    variants={variants}
+                    initial='hidden'
+                    animate='enter'
+                    exit='exit'
+                    transition={{ ease: 'easeInOut', duration: 0.3 }}
+                  >
+                    <DefaultSeo
+                      titleTemplate='%s - Taufik Hidayat'
+                      defaultTitle={metaTagsData.title}
+                      description={metaTagsData.deskripsi}
+                      twitter={{
+                        cardType: 'summary_large_image',
+                        site: '@tfkhdyt142',
+                        handle: '@tfkhdyt142',
+                      }}
+                      canonical={metaTagsData.link}
+                      openGraph={{
+                        url: metaTagsData.link,
+                        type: 'profile',
+                        title: metaTagsData.title,
+                        description: metaTagsData.deskripsi,
+                        images: [
+                          {
+                            url:
+                              metaTagsData.link + '/images/thumbnail-630.png',
+                            width: 1120,
+                            height: 630,
+                            alt: 'thumbnail 630p',
+                            type: 'image/png',
+                          },
+                          {
+                            url: metaTagsData.link + metaTagsData.thumbnail,
+                            width: 1280,
+                            height: 720,
+                            alt: 'thumbnail 720p',
+                            type: 'image/png',
+                          },
+                        ],
+                        site_name: metaTagsData.title,
+                      }}
+                    />
+                    <Component {...pageProps} />
+                  </m.div>
+                </AnimatePresence>
+              </LazyMotion>
+            </Layout>
+          </Provider>
         </NotificationsProvider>
       </MantineProvider>
     </ColorSchemeProvider>
