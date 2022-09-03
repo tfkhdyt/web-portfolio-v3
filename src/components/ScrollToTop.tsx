a5a004598aaa6cdb10c6c1f7bd695e33fbb143ca
tfkhdyt <tfkhdyt@pm.me>
2022-09-03 10:57:09 +0700

    fix(scroll to top): set right position to 30

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -6,7 +6,7 @@ function ScrollToTop() {
   const [scroll, scrollTo] = useWindowScroll();
 
   return (
-    <Affix position={{ bottom: 13, right: 20 }}>
+    <Affix position={{ bottom: 13, right: 30 }}>
       <Transition transition='fade' mounted={scroll.y > 0}>
         {(transitionStyles) => (
           <ActionIcon
