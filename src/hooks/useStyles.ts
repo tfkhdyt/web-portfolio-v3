609fcd72766cdf36105c501ab54223c4bd9f8337
tfkhdyt <tfkhdyt@pm.me>
2022-09-14 08:02:02 +0700

    fix(card style): change hover color on light theme

--- src/hooks/useStyles.ts
+++ src/hooks/useStyles.ts
@@ -10,8 +10,8 @@ export const useStyles = createStyles((theme) => ({
       backgroundColor:
         theme.colorScheme === 'dark'
           ? theme.colors.dark[6]
-          : theme.colors.gray[3],
-      transform: 'translateY(-8px)',
+          : theme.colors.gray[1],
+      transform: 'translateY(-10px)',
     },
   },
   gradientText: {
