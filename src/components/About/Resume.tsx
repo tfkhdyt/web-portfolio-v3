d0a7bb5c39438a42ddff6249a7381773896d26dd
tfkhdyt <tfkhdyt@pm.me>
2022-10-12 09:57:07 +0700

    chore(resume): update resume file

--- src/components/About/Resume.tsx
+++ src/components/About/Resume.tsx
@@ -3,13 +3,8 @@ import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 import { Box, Text, useMantineColorScheme } from '@mantine/core';
 import { Viewer } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-import { FC } from 'react';
 
-interface Props {
-  link: string;
-}
-
-const Resume: FC<Props> = () => {
+const Resume = () => {
   const defaultLayoutPluginInstance = defaultLayoutPlugin({
     sidebarTabs: (defaultTabs) => [defaultTabs[0]],
   });
@@ -20,31 +15,6 @@ const Resume: FC<Props> = () => {
       <Text size={42} weight='bold' mb='md'>
         Resume
       </Text>
-      {/*   <Text>
-        Also you can read my resume{' '}
-        <Link href={link} passHref>
-          <Anchor
-            color={theme.colorScheme === 'dark' ? 'gray.0' : 'dark'}
-            target='_blank'
-            weight='bold'
-            component='a'
-          >
-            here
-          </Anchor>
-        </Link>
-      </Text> */}
-      {/* <Text size={42} weight='bold'>
-        Resume
-      </Text>
-      <Group position='center'>
-        <iframe
-          src={src}
-          width='640'
-          height='480'
-          allow='autoplay'
-          loading='lazy'
-        />
-      </Group> */}
       <Box
         sx={{
           ['@media (max-width: 768px)']: {
