44ba7a07a009ae53b32961e847cfebc63c587be8
tfkhdyt <tfkhdyt@pm.me>
2022-11-15 07:59:48 +0700

    feat(document): add lang attribute to html tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,7 @@ export default class _Document extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang='en'>
         <Head>
           {/* <link
             href='https://fontbit.io/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap'
