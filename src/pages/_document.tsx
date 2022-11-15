edf56725b8be3cd233baa94b97edea153422eee0
tfkhdyt <tfkhdyt@pm.me>
2022-11-15 07:19:26 +0700

    feat(fonts): migrate to @next/font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,10 +16,10 @@ export default class _Document extends Document {
             href='https://fontbit.io/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap'
             rel='stylesheet'
           /> */}
-          <link
+          {/*   <link
             href='https://fontbit.io/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
             rel='stylesheet'
-          />
+          /> */}
           <link
             rel='shortcut icon'
             href={metaTagsData.favicon}
