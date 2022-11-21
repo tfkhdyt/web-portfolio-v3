24ed2a6173153a9e81626296ad333a54a1530143
tfkhdyt <tfkhdyt@pm.me>
2022-11-22 06:46:35 +0700

    feat(contacts data): add matrix, remove twitter and showwcase, and reorder

--- src/data/Contact/contacts.ts
+++ src/data/Contact/contacts.ts
@@ -2,39 +2,14 @@ import { Contact } from './contact';
 
 export const contacts: Contact[] = [
   {
-    name: 'Facebook',
-    label: 'Taufik Hidayat',
-    link: 'https://www.facebook.com/tfkhdyt142/',
+    name: 'Mastodon',
+    label: '@tfkhdyt@fosstodon.org',
+    link: 'https://fosstodon.org/@tfkhdyt',
   },
   {
-    name: 'Twitter',
-    label: '@tfkhdyt142',
-    link: 'https://twitter.com/tfkhdyt142',
-  },
-  {
-    name: 'Instagram',
-    label: '@_tfkhdyt_',
-    link: 'https://www.instagram.com/_tfkhdyt_/',
-  },
-  {
-    name: 'YouTube',
-    label: 'tfkhdyt',
-    link: 'https://youtube.com/tfkhdyt',
-  },
-  {
-    name: 'Telegram',
-    label: '@tfkhdyt',
-    link: 'https://t.me/tfkhdyt',
-  },
-  {
-    name: 'LinkedIn',
-    label: 'Taufik Hidayat',
-    link: 'https://www.linkedin.com/in/tfkhdyt142/',
-  },
-  {
-    name: 'Email',
-    label: 'tfkhdyt@proton.me',
-    link: 'mailto:tfkhdyt@proton.me',
+    name: 'Matrix',
+    label: '@tfkhdyt:matrix.org',
+    link: 'https://matrix.to/#/@tfkhdyt:matrix.org',
   },
   {
     name: 'GitHub',
@@ -42,13 +17,43 @@ export const contacts: Contact[] = [
     link: 'https://github.com/tfkhdyt',
   },
   {
-    name: 'Showwcase',
-    label: 'tfkhdyt',
-    link: 'https://tfkhdyt.showwcase.com/',
+    name: 'Telegram',
+    label: '@tfkhdyt',
+    link: 'https://t.me/tfkhdyt',
   },
   {
-    name: 'Mastodon',
-    label: '@tfkhdyt@fosstodon.org',
-    link: 'https://fosstodon.org/@tfkhdyt',
+    name: 'Email',
+    label: 'tfkhdyt@proton.me',
+    link: 'mailto:tfkhdyt@proton.me',
   },
+  {
+    name: 'Facebook',
+    label: 'Taufik Hidayat',
+    link: 'https://www.facebook.com/tfkhdyt142/',
+  },
+  {
+    name: 'YouTube',
+    label: 'tfkhdyt',
+    link: 'https://youtube.com/tfkhdyt',
+  },
+  // {
+  //   name: 'Twitter',
+  //   label: '@tfkhdyt142',
+  //   link: 'https://twitter.com/tfkhdyt142',
+  // },
+  {
+    name: 'Instagram',
+    label: '@_tfkhdyt_',
+    link: 'https://www.instagram.com/_tfkhdyt_/',
+  },
+  {
+    name: 'LinkedIn',
+    label: 'Taufik Hidayat',
+    link: 'https://www.linkedin.com/in/tfkhdyt142/',
+  },
+  // {
+  //   name: 'Showwcase',
+  //   label: 'tfkhdyt',
+  //   link: 'https://tfkhdyt.showwcase.com/',
+  // },
 ];
