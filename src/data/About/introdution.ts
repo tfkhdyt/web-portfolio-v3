7ce982d1e93a5ed498862e2794e997103ce4e967
tfkhdyt <tfkhdyt@proton.me>
2022-11-30 11:36:09 +0700

    fix(introduction): change some term

--- src/data/About/introdution.ts
+++ src/data/About/introdution.ts
@@ -1,5 +1,5 @@
 export const introduction: string[] = [
-  "Hi everyone, my name is Taufik Hidayat. I'm a Junior Full Stack Developer, Computer Science Student, YouTuber, Free/Libre and Open Source Software Enthusiast, Linux User, and Google-fu.",
+  "Hi everyone, my name is Taufik Hidayat. I'm a Junior Full Stack Developer, Computer Science Student, YouTuber, Free and Open Source Software Enthusiast, and Linux Nerd",
   "I live in Bandung, Indonesia. I was born in Majalengka, 1 April 2002. I'm 20 years old. I'm a Computer Science Student from Bale Bandung University.",
   'Coding has been my passion and hobby since I was 15 years old, and I love computer since I was a kid. Besides coding, I love listening to music. My favorite genres are Pop Punk, Post-hardcore, Metalcore, Nu-metal, and Electronicore. I like playing games too. My favorite genre is First Person Shooter',
   'Sometimes I watch anime, but I prefer watching western movies and series, especially MCU (Marvel Cinematic Universe). I spend a lot of time on Facebook sharing memes and shitposts.',
