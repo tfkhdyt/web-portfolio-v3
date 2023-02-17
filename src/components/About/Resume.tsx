afc3f721e81fa88e43ca488e475ab0776d039f57
tfkhdyt <tfkhdyt@proton.me>
2023-02-17 21:32:28 +0700

    fix(resume): remove resume preview

--- src/components/About/Resume.tsx
+++ src/components/About/Resume.tsx
@@ -1,33 +1,26 @@
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-
-import { Box, Text, useMantineColorScheme } from '@mantine/core';
-import { Viewer } from '@react-pdf-viewer/core';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import { Box, Text } from '@mantine/core';
+import Link from 'next/link';
 
 const Resume = () => {
-  const defaultLayoutPluginInstance = defaultLayoutPlugin({
-    sidebarTabs: (defaultTabs) => [defaultTabs[0]],
-  });
-  const { colorScheme } = useMantineColorScheme();
-
   return (
     <Box mt='md' id='resume'>
       <Text size={42} weight='bold' mb='md'>
         Resume
       </Text>
       <Box
-        sx={{
-          ['@media (max-width: 768px)']: {
-            height: 450,
-          },
-          height: 700,
-        }}
+      // sx={{
+      //   ['@media (max-width: 768px)']: {
+      //     height: 450,
+      //   },
+      //   height: 700,
+      // }}
       >
-        <Viewer
-          fileUrl='/pdf/resume.pdf'
-          plugins={[defaultLayoutPluginInstance]}
-          theme={colorScheme}
-        />
+        {/* <Viewer */}
+        {/*   fileUrl='/pdf/resume.pdf' */}
+        {/*   plugins={[defaultLayoutPluginInstance]} */}
+        {/*   theme={colorScheme} */}
+        {/* /> */}
+        You can read my resume <Link href='/pdf/resume.pdf'>here.</Link>
       </Box>
     </Box>
   );
