1d77488963d1e63892499876fcae61a68bf18c47
tfkhdyt <tfkhdyt@proton.me>
2023-03-15 15:16:04 +0700

    style(data): format

--- src/data/Projects/project.d.ts
+++ src/data/Projects/project.d.ts
@@ -3,9 +3,9 @@ import { skills } from './Skills/skills';
 
 export interface Project {
   name: string;
-  category: typeof projectCategories[number];
+  category: (typeof projectCategories)[number];
   desc: string;
-  tech: typeof skills[number]['name'][];
+  tech: (typeof skills)[number]['name'][];
   link: {
     demo: string | null;
     repo: string | null;
--- src/data/Skills/skill.d.ts
+++ src/data/Skills/skill.d.ts
@@ -2,7 +2,7 @@ import { skillCategories } from './skillCategories';
 
 export interface Skill {
   name: string;
-  category: typeof skillCategories[number];
+  category: (typeof skillCategories)[number];
   icon: string;
   show?: boolean;
 }
